6b6c4f2bc3fa9e0d31f0549edd26ba938e41745a
Andrew Ivasiv <andrew.ivasiv@gmail.com>
2019-09-07 23:34:30 +0300

    Add destiny API request on the backend

--- server/src/server.js
+++ server/src/server.js
@@ -7,7 +7,14 @@ const port = process.env.PORT || "8000";
 const dotenv = require("dotenv").config();
 const fetch = require("node-fetch");
 
+// Constants
 const REACT_APP_URL = 'http://localhost:1234';
+
+const ErrorCode = {
+  NONE: 0,
+  SUCCESS: 1,
+  UNAVAILABLE: 5
+}
 
 // TODO figure out how to store this and the refresh token
 var ACCESS_TOKEN = '';
@@ -46,6 +53,40 @@ app.get("/oauth/redirect", (req, res) => {
           res.redirect(`${REACT_APP_URL}/profile/${data.Response.destinyMemberships[0].membershipId}`);
         })
     })
+});
+
+app.get("/destiny/:profileId", (req, res) => {
+  // Components:
+  // 100 - profile data (character IDs)
+  // 102 - profile inventorie (vault)
+  // 201 - character inventorie
+  // 202 - character progression (season resets)
+  // 205 - character equipment
+  // 300 - basic instanced item info
+  // 305 - instanced item perk info
+  // 700 - presentation nodes (seals)
+  // 800 - collectibles
+  // 900 - triumphs
+  fetch(`https://www.bungie.net/Platform//Destiny2/4/Profile/${req.params.profileId}/?components=100,102,201,202,205,300,305,700,800,900`,
+    {
+      method: 'GET',
+      headers: {
+        'x-api-key': process.env.BUNGIE_API_KEY,
+        'Authorization': `Bearer ${ACCESS_TOKEN}`
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.ErrorCode != ErrorCode.SUCCESS) {
+        res.status(400).json({ 'errorMessage': data.Message });
+      }
+
+      res.status(200).json({
+        'profileCollectibles': data.Response.profileCollectibles.data.collectibles,
+        'profileRecords': data.Response.profileRecords.data.records,
+        'charRecords': data.Response.profileRecords.data,
+      });
+    });
 });
 
 app.listen(port)
--- view/src/Profile.js
+++ view/src/Profile.js
@@ -1,20 +1,39 @@
 import React from "react";
 
+import { SERVER_URL } from './constants.js';
+
 class Profile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      destinyData: null
     };
   }
 
   componentDidMount() {
-
+    fetch(`${SERVER_URL}/destiny/${this.props.match.params.profileId}`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.errorMessage) {
+          // TODO indicate error to user
+          console.log(data.errorMessage);
+          return;
+        }
+        this.setState({ destinyData: data })
+      });
   }
 
   render() {
-    return (
-      <div>Profile: {this.props.match.params.profileId}</div>
-    );
+    if (this.state.destinyData) {
+      return (
+        <div>Profile: {this.props.match.params.profileId}</div>
+      );
+    }
+    else {
+      return (
+        <div>Loading...</div>
+      );
+    }
   }
 }
 
